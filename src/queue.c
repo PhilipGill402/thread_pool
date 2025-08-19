d690d52117015965b2c4998d8d7df57b968cef4c
FurryBasket9978 <pgillbama@icloud.com>
2025-08-19 18:26:08 -0500

    started work on implementing queues

--- src/queue.c
+++ src/queue.c
@@ -7,3 +7,29 @@ struct job_queue{
     int tail;
     int num_jobs;
 };
+
+struct job_queue* init_queue(job_queue* queue){
+    queue->buffer = malloc(sizeof(struct job) * 10);
+    queue->capacity = 10;
+    queue->head = 0;
+    queue->tail = 0;
+    queue->num_jobs = 0;
+}
+
+
+
+void append_queue(struct job_queue* queue, struct job* job){
+    if (num_jobs > capacity){
+        queue->buffer = realloc(sizeof(struct job) * capacity * 2);
+    }
+
+
+}
+
+int main(){
+    struct job_queue q;
+    struct job_queue* queue = init_queue(&q);
+}
+
+
+
