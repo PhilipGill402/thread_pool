ec3c8723672f33323bd19392c8194f74fc67aa06
FurryBasket9978 <pgillbama@icloud.com>
2025-08-23 13:39:49 -0500

    started working on the thread pool init function but gave up lol

--- src/pool.c
+++ src/pool.c
@@ -3,14 +3,16 @@
 struct thread_pool* init_tp(int num_threads, int queue_capacity){
     thread_pool* tp = malloc(sizeof(thread_pool*));
     tp->workers = malloc(sizeof(pthread_t*) * num_threads);
-    tp->num_workers = 
-    tp->queue = init_queue(queue_capacity);
-    tp->m = 
-    tp->not_empty = 
-    tp->not_full = 
+    tp->num_workers = num_threads; 
+    tp->queue = *init_queue(queue_capacity);
+    pthread_mutex_init(&(tp->m), NULL);
+    pthread_cond_init(&(tp->not_empty), NULL);
+    pthread_cond_init(&(tp->not_full), NULL);
     tp->accepting = 1;
     tp->shutdown_requested = 0; 
     tp->graceful = 1;
+
+    return tp;
 }
 
 int submit_job(struct thread_pool* tp, void (*fn)(void*), void* arg){
